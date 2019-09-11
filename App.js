a92101c485f2869bb74a853bddfdf6ee74167402
prashantbhat84 <prashantbhat84@gmail.com>
2019-09-11 13:16:10 +0530

    Navigation setup and Home screen(Products Overview)

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer
@@ -12,9 +13,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={}></View>
+      <ShopNavigator />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({});
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, Text } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 
-const ProductsOverviewScreen = props => {};
+const ProductsOverviewScreen = props => {
+  const products = useSelector(state => state.products.availableProducts);
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={itemData => <Text>{itemData.item.title}</Text>}
+    />
+  );
+};
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: 'Products Overview'
+};
 
 const styles = StyleSheet.create({});
 
